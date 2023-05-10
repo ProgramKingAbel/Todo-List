c6468b91bf63a545817b7e3bacf6dfc07efa389b
ProgramKingAbel <abelmorara254@gmail.com>
2023-05-10 22:03:50 +0100

    FIXED ESLINT ERROR

--- src/index.js
+++ src/index.js
@@ -2,30 +2,29 @@ import './styles/main.css';
 
 const taskContainer = document.querySelector('.all-tasks');
 const tasks = [
-    {
-        id: Date.now(),
-        completed: false,
-        description: 'Make my meals',
-     },
-    {
-        id: Date.now(),
-        completed: false,
-        description: 'Take kids to school',
-     },
-    {
-        id: Date.now(),
-        completed: false,
-        description: 'Report to Work',
-     },
-    {
-        id: Date.now(),
-        completed: false,
-        description: 'Be happy to Code',
-     },
+  {
+    id: Date.now(),
+    completed: false,
+    description: 'Make my meals',
+  },
+  {
+    id: Date.now(),
+    completed: false,
+    description: 'Take kids to school',
+  },
+  {
+    id: Date.now(),
+    completed: false,
+    description: 'Report to Work',
+  },
+  {
+    id: Date.now(),
+    completed: false,
+    description: 'Be happy to Code',
+  },
 ];
 
 tasks.forEach((task, e) => {
-    
   const taskItem = document.createElement('div');
   taskItem.classList = 'list-item';
 
@@ -34,18 +33,16 @@ tasks.forEach((task, e) => {
 
   const taskDesc = document.createElement('input');
   taskDesc.type = 'text';
-  taskDesc.value = `${tasks[e].description}`
-
+  taskDesc.value = `${tasks[e].description}`;
 
   const ellipses = document.createElement('button');
   ellipses.innerHTML = '<i class="uil uil-ellipsis-v"></i>';
 
   taskItem.append(checkbox);
   taskItem.append(taskDesc);
-    taskItem.append(ellipses);
-    
-    taskContainer.append(taskItem);
-    taskDesc.removeAttribute('disabled');
-    taskDesc.focus();
-});
+  taskItem.append(ellipses);
 
+  taskContainer.append(taskItem);
+  taskDesc.removeAttribute('disabled');
+  taskDesc.focus();
+});
