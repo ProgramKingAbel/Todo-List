585ec93c8240ac4ddb596f15c564892583e752f5
ProgramKingAbel <abelmorara254@gmail.com>
2023-05-10 18:04:43 +0100

    dynamically generate list items

--- src/index.js
+++ src/index.js
@@ -24,3 +24,53 @@ const createMyTask = () => {
     
 }
 
+createTask.addEventListener('click', (e) => {
+    e.preventDefault();
+    const myTask = formInput.value;
+    if (myTask) {
+        createMyTask();
+        form.reset();
+    }
+});
+
+const generateTask = myTask => {
+ 
+    const taskItem = document.createElement('div');
+    taskItem.classList = 'list-item';
+
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.checked = myTask.completed;
+
+    if (myTask.completed) {
+        taskItem.classList.add('done');
+    }
+
+    const taskDesc = document.createElement('input');
+    taskDesc.type = "text";
+    taskDesc.value= myTask.description;   
+    taskDesc.setAttribute("disabled", "");
+
+    const ellipses = document.createElement('button');
+    ellipses.innerHTML = `<i class="uil uil-ellipsis-v"></i>`;
+    console.log(ellipses);
+
+    taskItem.append(checkbox);
+    taskItem.append(taskDesc);
+    taskItem.append(ellipses);
+    
+    checkbox.addEventListener("change", () => {
+        myTask.completed = checkbox.checked;
+        if (myTask.completed) {
+            taskItem.classList.add('done');
+        } else {
+            taskItem.classList.remove('done');
+        }
+ 
+    });
+
+
+    
+    return { taskItem, taskDesc, ellipses }
+}
+
