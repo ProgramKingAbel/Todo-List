7e47350f4a0e621dffd34fe4fe4e685bba770e23
ProgramKingAbel <abelmorara254@gmail.com>
2023-05-10 21:59:43 +0100

    REFACTOR TO USE HARDCODED ARRAY OBJECT FOR TASKS

--- src/index.js
+++ src/index.js
@@ -1,80 +1,51 @@
 import './styles/main.css';
 
 const taskContainer = document.querySelector('.all-tasks');
-const createTask = document.getElementById('create');
-const formInput = document.querySelector('.input');
-const form = document.querySelector('form');
-const tasks = [];
+const tasks = [
+    {
+        id: Date.now(),
+        completed: false,
+        description: 'Make my meals',
+     },
+    {
+        id: Date.now(),
+        completed: false,
+        description: 'Take kids to school',
+     },
+    {
+        id: Date.now(),
+        completed: false,
+        description: 'Report to Work',
+     },
+    {
+        id: Date.now(),
+        completed: false,
+        description: 'Be happy to Code',
+     },
+];
 
-const generateTask = (myTask) => {
+tasks.forEach((task, e) => {
+    
   const taskItem = document.createElement('div');
   taskItem.classList = 'list-item';
 
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
-  checkbox.checked = myTask.completed;
-
-  if (myTask.completed) {
-    taskItem.classList.add('done');
-  }
 
   const taskDesc = document.createElement('input');
   taskDesc.type = 'text';
-  taskDesc.value = myTask.description;
-  taskDesc.setAttribute('disabled', '');
+  taskDesc.value = `${tasks[e].description}`
+
 
   const ellipses = document.createElement('button');
   ellipses.innerHTML = '<i class="uil uil-ellipsis-v"></i>';
 
   taskItem.append(checkbox);
   taskItem.append(taskDesc);
-  taskItem.append(ellipses);
-
-  checkbox.addEventListener('change', () => {
-    myTask.completed = checkbox.checked;
-    if (myTask.completed) {
-      taskItem.classList.add('done');
-    } else {
-      taskItem.classList.remove('done');
-    }
-  });
-
-  taskDesc.addEventListener('input', () => {
-    myTask.description = formInput.value;
-  });
-
-  taskDesc.addEventListener('blur', () => {
-    taskDesc.setAttribute('disabled', '');
-  });
-  taskDesc.addEventListener('hover', () => {
+    taskItem.append(ellipses);
+    
+    taskContainer.append(taskItem);
     taskDesc.removeAttribute('disabled');
     taskDesc.focus();
-  });
-
-  return { taskItem, taskDesc, ellipses };
-};
-
-const createMyTask = () => {
-  const myTask = {
-    id: Date.now(),
-    completed: false,
-    description: formInput.value,
-  };
-
-  tasks.push(myTask);
-
-  const { taskItem, taskDesc } = generateTask(myTask);
-
-  taskContainer.append(taskItem);
-  taskDesc.removeAttribute('disabled');
-  taskDesc.focus();
-};
-
-createTask.addEventListener('click', (e) => {
-  e.preventDefault();
-  const myTask = formInput.value;
-  if (myTask) {
-    createMyTask();
-    form.reset();
-  }
 });
+
