bc3e356414cce492a15dd3e982c52e9e68cf703c
ProgramKingAbel <abelmorara254@gmail.com>
2023-05-19 12:35:05 +0100

    Fix Merge Conflict and Linters

--- src/modules/clearCompleted.test.js
+++ src/modules/clearCompleted.test.js
@@ -1,5 +1,5 @@
 import clearCompleted from './clearCompleted.js';
-import save from "./filtered.js";
+import save from './filtered.js';
 
 describe('clearCompleted', () => {
   test('should remove completed tasks', () => {
@@ -13,37 +13,34 @@ describe('clearCompleted', () => {
   });
 });
 
-
-
 beforeEach(() => {
-    const testTasks = [
-        {
-            id: 1,
-            description: 'Task 1',
-            completed: false,
-        },
-        {
-            id: 2,
-            description: 'Task 1',
-            completed: true,
-        },
-        {
-            id: 3,
-            description: 'Task 1',
-            completed: true,
-        }
-    ]
-    localStorage.setItem('tasks', JSON.stringify(testTasks));
-})
+  const testTasks = [
+    {
+      id: 1,
+      description: 'Task 1',
+      completed: false,
+    },
+    {
+      id: 2,
+      description: 'Task 1',
+      completed: true,
+    },
+    {
+      id: 3,
+      description: 'Task 1',
+      completed: true,
+    },
+  ];
+  localStorage.setItem('tasks', JSON.stringify(testTasks));
+});
 
 describe('Clear completed', () => {
-    test('Remove completed from storage', () => {
-        clearCompleted();
-        let newTasks = localStorage.getItem('tasks');
-        newTasks = JSON.parse(newTasks);
-        const saved = save(newTasks);
-        expect(saved.length).toBe(1);
-        expect(saved[0].completed).toBeFalsy();
-    })
-})
-
+  test('Remove completed from storage', () => {
+    clearCompleted();
+    let newTasks = localStorage.getItem('tasks');
+    newTasks = JSON.parse(newTasks);
+    const saved = save(newTasks);
+    expect(saved.length).toBe(1);
+    expect(saved[0].completed).toBeFalsy();
+  });
+});
